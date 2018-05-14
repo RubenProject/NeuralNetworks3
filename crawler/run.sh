30b3c85097cba3dc9f1d8169352a9a0549460576
rubenproject <r.vdwaal1995@gmail.com>
2018-05-15 00:49:32 +0200

    re-encoding dirty

---
+++ crawler/run.sh
@@ -0,0 +1,4 @@
+./webspider https://www.nytimes.com data2.txt
+./webspider https://www.abcnews.go.com data3.txt
+./webspider https://www.huffingtonpost.com data1.txt
+./webspider https://www.polygon.com data0.txt
